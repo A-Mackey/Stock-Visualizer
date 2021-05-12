fa3fb1ab425571168397e980012a22ca2839ec62
Aidan Mackey <amackey@scu.edu>
2021-05-11 20:54:55 -0700

    Got rid of first screen

---
+++ components/Stock.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        <h1>{{name}} : {{symbol}}</h1>
+    </v-container>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+
+  props: {
+      name: String,
+      symbol: String,
+  }
+}
+</script>
